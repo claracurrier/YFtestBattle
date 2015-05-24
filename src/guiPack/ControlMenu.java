7574f349a4f97043d7239d8ca0d386e2102b7f94
Clara Currier <claracurrier@live.com>
2015-05-23 23:11:01 -0400

    Controls text

--- src/guiPack/ControlMenu.java
+++ src/guiPack/ControlMenu.java
@@ -7,9 +7,11 @@ package guiPack;
 import com.jme3.app.Application;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.font.BitmapFont;
+import com.jme3.font.LineWrapMode;
 import com.jme3.input.event.MouseButtonEvent;
 import com.jme3.math.Vector2f;
 import tonegod.gui.controls.buttons.ButtonAdapter;
+import tonegod.gui.controls.text.Label;
 import tonegod.gui.controls.windows.Window;
 import tonegod.gui.core.Screen;
 
@@ -36,10 +38,23 @@ public class ControlMenu {
         win.setIsResizable(false);
         win.setIsMovable(false);
         win.setIgnoreMouse(true);
-        
+
+        Label txtbox = new Label(screen, "controltext", new Vector2f(15, 15),
+                new Vector2f(300, 200));
+        txtbox.setText(" To move: use the arrow keys or the WASD keys."
+                + "\n To attack as Dan: use the mouse button. Hold down to charge."
+                + "\n To attack as Kirith: use the i or o keys. Hold down to chage"
+                + "\n To switch characters: use the g key"
+                + "\n To pause the game: use the h key");
+        txtbox.setFont("Interface/Fonts/Arial.fnt");
+        txtbox.setTextAlign(BitmapFont.Align.Left);
+        txtbox.setTextWrap(LineWrapMode.Word);
+        txtbox.setFontSize(16f);
+        win.addChild(txtbox);
+
         //go back button
         ButtonAdapter goBackBtn = new ButtonAdapter(screen, "GoBack",
-                new Vector2f(15, 105)) {
+                new Vector2f(15, 200)) {
             @Override
             public void onButtonMouseLeftUp(MouseButtonEvent evt, boolean toggled) {
                 screen.removeElement(win);
